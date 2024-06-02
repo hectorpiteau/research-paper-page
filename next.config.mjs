7853d537b37aa62e3a2dd6060a5ae650cd0eadeb
Hector Piteau <hector.piteau@lab6.co.jp>
2024-06-02 11:45:05 +0200

    init.
    Signed-off-by: Hector Piteau <hector.piteau@lab6.co.jp>

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true,
+    },
+};
+  
+export default nextConfig;
